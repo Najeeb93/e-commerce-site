743b9948d2bd992050ad9dcb34f24af43941234e
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-29 20:39:00 +0500

    add to commit upload

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 
-let productDiv = document.querySelector(".Products");
+let productDiv = document.querySelector(".Product");
 
-let displayProduct = async () => {
-    // productDiv.innerHTML = "";
+let displayProduct=async ()=> {
+    productDiv.innerHTML = '';
     let productData = await fetch('https://fakestoreapi.com/products');
     let finalproduct = await productData.json();
     finalproduct.forEach(element => {
-  
-    })
+        productDiv.innerHTML += `<div class="ProductItems">
+        <img src="${element.image}" alt="">
+        <p>${element.price} | ${element.rating.rate}</p>
+        <h3>${element.title}</h3>
+      </div>`
+    });
 }
 
 displayProduct();
