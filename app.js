888dd99376844620109ad8f9dacc5d04f64393ab
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-07-29 19:32:16 +0500

    upload

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 
-let productDiv = document.querySelector(".products");
+let productDiv = document.querySelector(".Products");
 
 let displayProduct = async () => {
     productDiv.innerHTML = "";
     let product = await fetch('https://fakestoreapi.com/products');
     let finalproduct = await product.json();
     finalproduct.forEach(element => {
-        productDiv.innerHTML += `<div class="Product-Item">
-        <img scr="${element.image}" alt="">
-        <p>Price Rs. ${element.price} | ${element.rating.rate}
-        </p>
-        <h3>${element.title}</h3>
-        </div>` 
+        productDiv.innerHTML  
     });
 }
 
